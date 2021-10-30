dba9654e9250955d7b65c8014383977a0151146f
Lucas Neves <lucas54neves@gmail.com>
2021-10-30 20:51:05 -0300

    feat: add shell script

---
+++ src/main.sh
@@ -0,0 +1,18 @@
+for i in {1..10}; do
+    python3 main.py 1 10000
+done
+
+
+for i in {1..10}; do
+    python3 main.py 2 10000
+done
+
+
+for i in {1..10}; do
+    python3 main.py 4 1000
+done
+
+
+for i in {1..10}; do
+    python3 main.py 8 10000
+done
